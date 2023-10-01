dedbf5e37781da21bada44fc6e60f9b95a944b3a
Ely Lucas <ely@meta-tek.net>
2023-10-01 13:30:34 -0600

    Lesson 15: using zod to make schema

--- src/content/posts/config.ts
+++ src/content/posts/config.ts
@@ -1,6 +1,16 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
+import { format } from 'date-fns';
 
-const blogCollection = defineCollection({});
+const blogCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    author: z.string(),
+    categories: z.array(z.string()),
+    date: z.string().transform((str) => format(new Date(str), 'MMMM dd, yyyy')),
+    featured: z.boolean().default(false),
+    image: z.string(),
+  }),
+});
 
 export const collections = {
   posts: blogCollection,
