5517311a56bb3324256339a99f003c9988f0df75
Ely Lucas <ely@meta-tek.net>
2023-10-27 09:04:59 -0600

    switching to ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://rhythm.nation'
+  site: 'https://astro-course-demo-ivory.vercel.app',
+  output: 'server',
+  adapter: vercel()
 });
